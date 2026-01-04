8291ba0cc4d274bad6f5f30134d69d9148d2ca22
jc1471 <jordanbcrow@gmail.com>
2026-01-04 19:40:06 +0100

    Styling complete with hsla background gradient

--- react/fm-results-summary/components/CategoryList.jsx
+++ react/fm-results-summary/components/CategoryList.jsx
@@ -8,7 +8,12 @@ export default function CategoryList() {
     return (
         <>
             {data.map((item) => (
-                <CategoryBar key={item.category} icon={item.icon}  category={item.category} score={item.score} />))}
+                <CategoryBar
+                    key={item.category}
+                    icon={item.icon}
+                    category={item.category}
+                    score={item.score}
+                />))}
         </>
     )
 }
--- react/fm-results-summary/components/ScoreComparison.jsx
+++ react/fm-results-summary/components/ScoreComparison.jsx
@@ -3,8 +3,8 @@
 export default function ScoreComparison() {
     return (
         <div className="score-comparison">
-            <h3>Great/Good</h3>
-            <p>You scored higher than x% of the people who have taken these tests.</p>
+            <h3>Great</h3>
+            <p>You scored higher than 65% of the people who have taken these tests.</p>
         </div>
     )
 }
